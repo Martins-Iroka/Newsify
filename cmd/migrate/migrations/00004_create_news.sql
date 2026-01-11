f6344ed4ff486bff8350a26a42deb3ae1136f01a
Ikechukwu Iroka <martins.iroka@gmail.com>
2026-01-11 18:48:54 +0100

    feat: Implement CRUD operations for news articles in the database

--- cmd/migrate/migrations/00004_create_news.sql
+++ cmd/migrate/migrations/00004_create_news.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE IF NOT EXISTS news(
+CREATE TABLE IF NOT EXISTS news_article(
     id bigserial PRIMARY KEY,
     title TEXT NOT NULL,
     content TEXT NOT NULL,
     creator_id bigserial NOT NULL,
-    create_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
 -- +goose StatementEnd
 
--- internal/database/newsarticle/newsarticlestore.go
+++ internal/database/newsarticle/newsarticlestore.go
@@ -27,11 +27,38 @@ type NewsArticleStore struct {
 }
 
 func (na *NewsArticleStore) CreateNewsArticle(ctx context.Context, newsArticle *NewsArticle) error {
+	query := `
+		INSERT INTO news_article (title, content, creator_id) VALUES ($1, $2, $3) RETURNING id
+	`
+
+	ctx, cancel := context.WithTimeout(ctx, util.QueryTimeoutDuration)
+	defer cancel()
+
+	if err := na.DB.QueryRowContext(ctx, query, newsArticle.Title, newsArticle.Content, newsArticle.CreatorID).Scan(
+		&newsArticle.ID,
+	); err != nil {
+		return err
+	}
 	return nil
 }
 
-func (na *NewsArticleStore) GetNewsArticleById(ctx context.Context, creatorID int64, article int64) (*NewsArticle, error) {
-	return nil, nil
+func (na *NewsArticleStore) GetNewsArticleById(ctx context.Context, creatorID int64, articleID int64) (*NewsArticle, error) {
+	query := `
+		SELECT na.title, na.content, na.created_at FROM news_article na WHERE na.id = $1 AND na.creator_id = $2 ORDER BY na.created_at DESC
+	`
+
+	ctx, cancel := context.WithTimeout(ctx, util.QueryTimeoutDuration)
+	defer cancel()
+
+	var news NewsArticle
+	if err := na.DB.QueryRowContext(ctx, query, articleID, creatorID).Scan(
+		&news.Title,
+		&news.Content,
+		&news.CreatorID,
+	); err != nil {
+		return nil, err
+	}
+	return &news, nil
 }
 
 func (na *NewsArticleStore) GetAllNewsArticleByCreator(ctx context.Context, creatorID int64, pagination util.PaginatedPostQuery) ([]NewsArticle, error) {
@@ -39,6 +66,17 @@ func (na *NewsArticleStore) GetAllNewsArticleByCreator(ctx context.Context, crea
 }
 
 func (na *NewsArticleStore) DeleteNewsArticle(ctx context.Context, creatorID int64, articleID int64) error {
+	query := `
+		DELETE FROM news_article WHERE id = $1 AND creator_id = $2
+	`
+
+	ctx, cancel := context.WithTimeout(ctx, util.QueryTimeoutDuration)
+	defer cancel()
+
+	_, err := na.DB.ExecContext(ctx, query, articleID, creatorID)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
